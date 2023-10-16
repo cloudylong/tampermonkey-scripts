e095ea4739cbbf0e7339ec491552a8cd6acfca0e
Walter <walter.yl.wang@outlook.com>
2023-10-16 10:46:06 +0800

    B 站：倍速播放选项

---
+++ bilibili-speed.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Bilibili 倍速播放
+// @namespace    https://github.com/cloudylong/
+// @version      1.0
+// @description  增加B站的倍速播放选项
+// @author       Walter
+// @match        https://www.bilibili.com/video/*
+// @icon         https://www.bilibili.com/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // 监听倍速列表的生成
+  const intervalID = setInterval(() => {
+    // 查找2倍速选项
+    const rate_2 = document.querySelector(
+      "li.bpx-player-ctrl-playbackrate-menu-item"
+    );
+    if (rate_2) {
+      // 移除定时器
+      console.log("hahahhaha");
+      clearInterval(intervalID);
+
+      // 动态生成3倍速选项
+      const rate_3 = rate_2.cloneNode(false);
+      rate_3.setAttribute("data-value", "3");
+      rate_3.innerText = "3.0x";
+      rate_2.parentElement.insertBefore(rate_3, rate_2);
+    }
+  }, 200);
+})();
